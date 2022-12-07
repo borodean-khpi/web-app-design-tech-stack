7950e49c4d3fc172fb7caab4feab05c19656362b
Vadym Borodin <borodean@gmail.com>
2022-12-07 14:36:56 +0200

    Posters are connections now

--- lab-3/example.php
+++ lab-3/example.php
@@ -3,8 +3,8 @@
 require_once 'Client.php';
 require_once 'Poster.php';
 
-$facebookConnection = new FacebookPoster('NoobMaster69', 'jelly22fi$h');
-$facebookConnection->post('Hello Facebook!');
+$facebookPoster = new FacebookPoster('NoobMaster69', 'jelly22fi$h');
+$facebookPoster->post('Hello Facebook!');
 
-$linkedInConnection = new LinkedInPoster('john.doe@example.com', 'jelly22fi$h');
-$linkedInConnection->post('Hello LinkedIn!');
+$linkedInPoster = new LinkedInPoster('john.doe@example.com', 'jelly22fi$h');
+$linkedInPoster->post('Hello LinkedIn!');
